21c911831e1b4ba379b3a140f03c8752bee91f5a
Abhinav Dadhich <abhinavvicku@gmail.com>
2014-05-03 02:03:58 +0900

    with occupancy update and ISM

--- octree.py
+++ octree.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import numpy as np
 
 # ----------------------------------Leaf class -----------------------
 class leaf:
@@ -15,7 +15,7 @@ class node:
 		self.parent = parent 
 		self.size = size 
 		self.coord = coord
-		self.value = []
+		self.value = 0.5
 		#self.center = center
 		self.children = [None for i in range(8)]
 
@@ -113,18 +113,22 @@ class Octree:
                         return newcenter
 
 	#--------------------------Inverse Sensor Model-------------
-	def ISM(self, coord):
-		mu = coord
-		sigma = [1,1,5]
-		pass
+	def ISM(self, curr,prev):
+		if prev>=0.9 and curr ==1.0:
+			return 0.95
+		else:
+			return 0.5
 		
 		
 
 	#--------------------------Occupancy update ---------------------
 	def occupancyUpdate(self,cur,prev):
-		logCur = np.log(cur/1.0-cur)
-		logprev = np.log(prev/1-prev)
-		pass
+		logCur = 5.0
+		#logprev = np.log(prev/1-prev)
+		logIsm = np.log(self.ISM(int(cur),int(prev))/(1-self.ISM(int(cur),int(prev))))
+		update = logCur + logIsm
+		print update
+		return (1.0-(1.0/(1.0+np.exp(update))))
 
 	#------------------------insert a leaf into tree ---------------
 
@@ -134,11 +138,13 @@ class Octree:
 		if size[0] == self.res or size[1]==self.res or size[2]==self.res:
 			# if of lowest size return leaf at that point
 			if parent.value != None:
-				parent.value = val #self.occupancyUpdate(curr.value,curr.coord)
-				self.arr.append([parent.coord,coord,val])
+				print parent.value
+				parent.value = self.occupancyUpdate(parent.value,1.0)
+				print parent.value
+				self.arr.append([parent.coord,coord,parent.value])
 			else:
 				branch = self.findQuadrant(coord,parent.coord)
-				parent[branch]= leaf(coord,val,res)
+				parent[branch]= leaf(coord,int(val),res)
 				self.arr.append([parent.coord,coord,val])
 				
 		else:
